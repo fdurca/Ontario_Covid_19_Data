608ec289337d8a58d04eae4ebc0421f22323a9eb
fdurca <filip.durca@gmail.com>
2020-11-10 17:00:19 -0500

    Functional Data Scraping

--- Main.py
+++ Main.py
@@ -11,6 +11,29 @@ request = requests.get(URL)
 data = request.json();
 d = data.get("result")
 records = d.get("records")
+groups = {}
+groups["Ontario"] = 0;
 
+# Outer loop
+while(True):
+    # Get the sort parameter
+    sort = input("Sort: ")
 
-for record in records:
+    if (sort == "exit"):
+        exit(0)
+
+    # Loop through each record
+    for record in records:
+        #if (record.get("Outcome1") == "Not Resolved"):
+        if record.get(sort) not in groups:
+            groups[record.get(sort)] = 1
+        else:
+            groups[record.get(sort)] = groups[record.get(sort)] + 1
+        groups["Ontario"] = groups["Ontario"] + 1
+
+    # Print
+    print("Sorted by: ", sort)
+    for s in sorted(groups, key = groups.get, reverse = True):
+        print('{:20s}{:7d}'.format(s, groups.get(s)))
+
+    print("-------------------------------------------------------------")
