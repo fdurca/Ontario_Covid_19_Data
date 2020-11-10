254a65763e6227e0e44e73f953ac2231a63c37dc
fdurca <filip.durca@gmail.com>
2020-11-10 02:22:11 -0500

    API Implementation

--- Main.py
+++ Main.py
@@ -1,5 +1,16 @@
 # @author Filip Durca
 # This is the main file that compiles the data
 
-# imports
+# import
+import requests
+import json
 
+# Variables
+URL = "https://data.ontario.ca/api/3/action/datastore_search?resource_id=455fd63b-603d-4608-8216-7d8647f43350&limit=100000"
+request = requests.get(URL)
+data = request.json();
+d = data.get("result")
+records = d.get("records")
+
+
+for record in records:
