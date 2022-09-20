0b687ebf85eaf11b7dd3169924f7bf2c8f3e6490
Filip Durca <filip.durca@gmail.com>
2022-09-20 18:04:05 -0400

    Final update

--- Main.py
+++ Main.py
@@ -6,7 +6,7 @@ import requests
 import json
 
 # Variables
-URL = "https://data.ontario.ca/api/3/action/datastore_search?resource_id=455fd63b-603d-4608-8216-7d8647f43350&limit=100000"
+URL = "https://data.ontario.ca/api/3/action/datastore_search?resource_id=455fd63b-603d-4608-8216-7d8647f43350&limit=300000"
 request = requests.get(URL)
 data = request.json();
 d = data.get("result")
@@ -24,16 +24,19 @@ while(True):
 
     # Loop through each record
     for record in records:
-        #if (record.get("Outcome1") == "Not Resolved"):
-        if record.get(sort) not in groups:
-            groups[record.get(sort)] = 1
-        else:
-            groups[record.get(sort)] = groups[record.get(sort)] + 1
-        groups["Ontario"] = groups["Ontario"] + 1
+        if record["Outcome1"] == "Not Resolved":
+            if record.get(sort) not in groups:
+                groups[record.get(sort)] = 1
+            else:
+                groups[record.get(sort)] = groups[record.get(sort)] + 1
+            groups["Ontario"] = groups["Ontario"] + 1
 
     # Print
     print("Sorted by: ", sort)
     for s in sorted(groups, key = groups.get, reverse = True):
-        print('{:20s}{:7d}'.format(s, groups.get(s)))
+        print('{:40s}{:7d}'.format(s, groups.get(s)))
 
     print("-------------------------------------------------------------")
+
+    groups.clear()
+    groups["Ontario"] = 0
